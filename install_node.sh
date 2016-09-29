0e50d676b57e203a3af9aa59b69242b8cd18c8d8
Nathan Baker <pcnate@gmail.com>
2016-09-29 05:57:03 +0000

    adding basics

---
+++ install_node.sh
@@ -0,0 +1,6 @@
+#! /bin/bash
+
+# source: https://nodejs.org/en/download/package-manager/#debian-and-ubuntu-based-linux-distributions
+
+curl -sL https://deb.nodesource.com/setup_6.x | sudo -E bash -
+sudo apt-get install -y nodejs build-essential
